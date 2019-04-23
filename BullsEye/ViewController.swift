bc35eee32c7b7d2bc22fcf370f9da7cf9a36edc1
Tadeu Cruz <tadeucruz@gmail.com>
2019-04-23 17:33:30 -0300

    ReIndent the code

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,57 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  var currentValue: Int = 0
-  @IBOutlet weak var slider: UISlider!
-  var targetValue: Int = 0
-  @IBOutlet weak var targetLabel: UILabel!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    currentValue = lroundf(slider.value)
-    startNewRound()
-  }
-
-  func updateLabels() {
-    targetLabel.text = String(targetValue)
-  }
-
-  func startNewRound() {
-    targetValue = 1 + Int(arc4random_uniform(100))
-    currentValue = 50
-    slider.value = Float(currentValue)
-    updateLabels()
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  @IBAction func sliderMoved(_ slider: UISlider) {
-    print("The value of slider is now: \(slider.value)")
-    currentValue = lroundf(slider.value)
-  }
-
-  @IBAction func showAlert() {
-
-    let difference: Int = abs(currentValue - targetValue)
-    let points = 100 - difference
-
-    let message = "You scored \(points) points"
-
-    let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
-
-    let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
-
-    alert.addAction(action)
-
-    present(alert, animated: true, completion: nil)
-
-    startNewRound()
-
-  }
-
+    
+    var currentValue = 0
+    @IBOutlet weak var slider: UISlider!
+    var targetValue = 0
+    @IBOutlet weak var targetLabel: UILabel!
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert() {
+        
+        let difference: Int = abs(currentValue - targetValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        
+    }
+    
 }
 
