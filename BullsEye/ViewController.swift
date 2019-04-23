b6aa8e87724742d13c179f40c326cc859114bbfa
Tadeu Cruz <tadeucruz@gmail.com>
2019-04-23 18:19:18 -0300

    Given bonus points

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -52,19 +52,26 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         let difference: Int = abs(currentValue - targetValue)
-        let points = 100 - difference
-        score += points
+        var points = 100 - difference
         
         let title: String
         if difference == 0 {
             title = "Perfect !"
+            points += 100
         } else if difference < 5 {
             title = "You almost had it !"
+            
+            if difference == 1 {
+                points += 50
+            }
+            
         } else if difference < 10 {
             title = "Pretty good !"
         } else {
             title = "Not even close..."
         }
+        
+        score += points
         
         let message = "You scored \(points) points"
         
